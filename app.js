f1ef2ba75d578b8d6e20527aac544711b745282e
Jorge Garcia <jorgegarcia525@gmail.com>
2018-07-15 11:30:11 -0500

    final changes

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var express = require("express");
 var app = express();
 var path = require("path");
-var formRouter = require('./routes/formRouter');
+//var formRouter = require('./routes/formRouter');
 
 app.use(express.static(__dirname + '/public'));
 
-app.use('/formSubmit', formRouter);
+//app.use('/formSubmit', formRouter);
 app.set("view engine", "ejs");
 
 
 app.get("/", function(req, res) {
 	res.render('template');
+});
+
+app.get("/home", function(req, res) {
+	res.render('template-update');
 });
 
 app.get("/form", function(req, res) {
