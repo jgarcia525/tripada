5d0e66b48b4a89bdd53a1f34f568641879b97716
Jorge Garcia <jorgegarcia525@gmail.com>
2018-10-12 11:13:06 -0500

    set up new architecture with working teamplate

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ var app = express();
 var path = require("path");
 //var formRouter = require('./routes/formRouter');
 
-app.use(express.static(__dirname + '/public'));
+
+app.use(express.static(__dirname + '/views/'));
 
 //app.use('/formSubmit', formRouter);
 app.set("view engine", "ejs");
 
 
 app.get("/", function(req, res) {
-	res.render('template');
+	res.render('home');
 });
 
 app.get("/home", function(req, res) {
